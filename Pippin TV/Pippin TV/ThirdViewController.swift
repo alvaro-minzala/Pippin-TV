36fbeb6ab69a9b36bd5a09c3f864689400747b1d
alvaro-minzala <71344754+alvaro-minzala@users.noreply.github.com>
2021-08-04 18:42:01 +0200

    Added a third view

---
+++ Pippin TV/Pippin TV/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  Pippin TV
+//
+//  Created by Alvaro Minzala on 4/8/21.
+//  Copyright © 2021 alvaro-minzala. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
